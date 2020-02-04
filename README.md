9a573786f10c0eb82f4449c34c4fdf8ab6697df8
aniket965 <aniket965.as@gmail.com>
2020-02-04 22:35:22 +0530

    feat:init

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # ipuresult-cli
+> ipuresult cli is tool for creating json files from pdf result files of [GGSIPU RESULTS](http://ggsipu.ac.in/ExamResults/ExamResultsmain.htm.
+
+> ipuresult-cli uses core libararies of [ipuresults](http://ipurhttps://ipur.herokuapp.com/) (launched in 2017) to extract results.
+
+## installation
+
+```sh
+npm install -g ipuresult-cli
+
+```
+
+## usage
+```sh
+ipuresult path_to_pdf_file output_directory
+```
+eg.
+
+```sh
+ipuresult ~/example/result.pdf ~/example/test/
+
+```
+
+for help you can contact at [email](mailto:aniket965.as@gmail.com)
--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 #! /usr/bin/env node
+
+// Copyright (c) 2020 aniket965
+// 
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+
+const { resultTxt2json } = require('./lib/resultExtractor');
+const fs = require('fs');
+const path = require('path')
+const PDFParser = require("pdf2json");
+
+
+const PDF_FILE_LOCATION = process.argv[2];
+const OUTPUT_FILE_LOCATION = process.argv[3] || '.';
+const RESULT_FILE_NAME = path.join(OUTPUT_FILE_LOCATION, 'result.json');
+
+const pdfParser = new PDFParser(this,1);
+
+pdfParser.on("pdfParser_dataError", errData => console.error(errData.parserError));
+
+pdfParser.on("pdfParser_dataReady", pdfData => {
+
+    const content = resultTxt2json(pdfParser.getRawTextContent());
+    if(content) fs.writeFileSync(RESULT_FILE_NAME,JSON.stringify(content));
+
+});
+
+pdfParser.loadPDF(PDF_FILE_LOCATION);
